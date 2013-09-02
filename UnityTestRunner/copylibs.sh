b8761af584d864c41381da8539d9cb58f0da1586
George Foot <george.foot@gmail.com>
2013-09-02 03:18:55 +0100

    Fixing up the Unity test runner a bit

--- UnityTestRunner/copylibs.sh
+++ UnityTestRunner/copylibs.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-cp -f ../bin/Debug/KopiLua.dll Assets
-cp -f ../bin/Debug/KopiLua.pdb Assets
-cp -f ../bin/Debug/KopiLuaTest.dll Assets
-cp -f ../bin/Debug/KopiLuaTest.pdb Assets
+DLLDIR=Assets/DLL
+cp -f ../bin/Debug/KopiLua.dll $DLLDIR
+cp -f ../bin/Debug/KopiLua.pdb $DLLDIR
+cp -f ../bin/Debug/KopiLuaTest.dll $DLLDIR
+cp -f ../bin/Debug/KopiLuaTest.pdb $DLLDIR
+cp -f ../nunitlite.dll $DLLDIR
 
-cd Assets
-pdb2mdb="/c/Program Files (x86)/Unity/Editor/Data/Mono/lib/mono/2.0/pdb2mdb.exe"
+cd $DLLDIR
+pdb2mdb="/c/Program Files (x86)/Unity4/Editor/Data/Mono/lib/mono/2.0/pdb2mdb.exe"
 "$pdb2mdb" KopiLua.dll
 "$pdb2mdb" KopiLuaTest.dll
 
